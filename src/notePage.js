cc6a5094a6b679078534c2b157ffedfd363be468
Lindsay Esterman <lindsayesterman@gmail.com>
2020-08-02 12:51:52 -0400

    note page delete

--- src/notePage.js
+++ src/notePage.js
@@ -1,17 +1,39 @@
 import React from 'react';
-import store from './store.js'
+import NotesContext from './notesContext'
+import { findNote } from './notes-helpers'
+import Note from './note'
 
-const { notes } = store
-export default function NotePage(props) {
-    const note = notes.find(p =>
-        p.id === props.match.params.noteId
-      )
+class NotePage extends React.Component{
+    static defaultProps = {
+        match: {
+          params: {}
+        }
+      }
+
+      handleDeleteNote = noteId => {
+        this.props.history.push(`/`)
+      }
+    
+      static contextType = NotesContext;
+
+    render(){
+    const { notes=[] } = this.context
+    const { noteId } = this.props.match.params
+    const note = findNote(notes, noteId) || { content: '' }
        return(
-           <div>
-                <h2>{note.name}</h2>
-                <p className="note-content">
-                    {note.content}
-                </p>
-           </div>
+           <div className="note-page">
+            <Note
+          id={note.id}
+          name={note.name}
+          modified={note.modified}
+          onDeleteNote={this.handleDeleteNote}
+        />
+          {note.content.split(/\n \r|\n/).map((para, i) =>
+            <p key={i}>{para}</p>
+          )}
+        </div>
        )
+    }
    }
+
+   export default NotePage;
