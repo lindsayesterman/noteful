bdcbf8bdcac89472ce87dd0abab394bda326241f
Lindsay Esterman <lindsayesterman@gmail.com>
2020-11-24 17:46:51 -0500

    preparing

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'https://sleepy-stream-22621.herokuapp.com/', 
+    API_ENDPOINT: 'https://sleepy-stream-22621.herokuapp.com', 
     API_KEY: '$2a$10$5a.oG5wjGK0C5doPBbKixuEEY6K.VfCMHmr3Dnc8aA9vf0Ws4oxHG'
   }
