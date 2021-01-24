bc1085f202fa9016c0e969ab3f9194712d6b5d7e
Lindsay Esterman <lindsayesterman@gmail.com>
2021-01-23 20:42:42 -0500

    isDev production var

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
+const isDev = process.env.NODE_ENV !== "production"
 export default {
-    // API_ENDPOINT: 'https://sleepy-stream-22621.herokuapp.com', 
-    API_ENDPOINT: 'http://localhost:8000/api',
+    API_ENDPOINT:  isDev  ? 'http://localhost:8000/api': "https://sleepy-stream-22621.herokuapp.com/api",
     API_KEY: '$2a$10$5a.oG5wjGK0C5doPBbKixuEEY6K.VfCMHmr3Dnc8aA9vf0Ws4oxHG'
   }
